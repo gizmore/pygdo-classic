33c32676abb3f16d2274161117de3fa0223e3871
gizmore <gizmore@wechall.net>
2024-05-15 20:14:50 +0200

    'account.settings'

--- module_classic.py
+++ module_classic.py
@@ -4,6 +4,6 @@ from gdo.base.GDO_Module import GDO_Module
 class module_classic(GDO_Module):
 
     def gdo_load_scripts(self, page):
-        self.add_css('css/pygdo_classic2.css')
+        self.add_css('css/pygdo_classic.css')
         self.add_css('css/pygdo_sidebar.css')
         self.add_js('css/pygdo_classic.js')
