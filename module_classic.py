0273386377b59bc5a9d2ef591c53ac1356eed547
gizmore <gizmore@wechall.net>
2024-12-20 02:25:43 +0100

    'pytest migration. async mysql'

--- module_classic.py
+++ module_classic.py
@@ -3,7 +3,7 @@ from gdo.base.GDO_Module import GDO_Module
 
 class module_classic(GDO_Module):
 
-    def gdo_load_scripts(self, page):
+    async def gdo_load_scripts(self, page):
         self.add_css('css/pygdo_classic.css')
         self.add_css('css/pygdo_sidebar.css')
         self.add_js('css/pygdo_classic.js')
